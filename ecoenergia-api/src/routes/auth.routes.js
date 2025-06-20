a5628a12b35d8ffae19620de56f1386dddd628ba
vfaels <faelsmelo15@gmail.com>
2025-06-20 12:59:42 -0400

    refactor: remover comentários desnecessários do middleware de autenticação e das rotas de autenticação

--- ecoenergia-api/src/routes/auth.routes.js
+++ ecoenergia-api/src/routes/auth.routes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
-// Rota para registrar um novo usuário
 router.post('/register', authController.register);
 
-// Rota para fazer login de um usuário
 router.post('/login', authController.login);
 
 module.exports = router;
