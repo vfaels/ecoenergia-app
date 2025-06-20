e90e04178f88d582c8f2bb4687a71bae510a06a7
vfaels <faelsmelo15@gmail.com>
2025-06-20 12:59:25 -0400

    feat: integrar gerenciamento de tema com styled-components e remover arquivo de formatadores

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+import { useTheme } from './contexts/themeContext';
 import { AppRouter } from './router/AppRouter';
 import { GlobalStyle } from './styles/GlobalStyle';
+import { lightTheme, darkTheme } from './styles/themes';
 
 function App() {
+  const { themeName } = useTheme();
+
   return (
-    <>
+    <ThemeProvider theme={themeName === 'light' ? lightTheme : darkTheme}>
       <GlobalStyle />
       <AppRouter />
-    </>
+    </ThemeProvider>
   );
 }
 
