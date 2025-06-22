a6d7c86db721d2598e299f800d5b9d6d8ad00112
vfaels <faelsmelo15@gmail.com>
2025-06-22 19:39:30 -0400

    feat: alterar configuração de upload para armazenamento em memória e definir limite de tamanho de arquivo

--- ecoenergia-api/src/config/upload.js
+++ ecoenergia-api/src/config/upload.js
@@ -6,13 +6,9 @@ const crypto = require('crypto');
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
 
 module.exports = {
-  directory: uploadFolder,
-  storage: multer.diskStorage({
-    destination: uploadFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
-      callback(null, fileName);
-    },
-  }),
+  storage: multer.memoryStorage(),
+
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
 };
