87dc4ba854fa99fbafdaca7273f0a1028e288dbc
Fael Melo <89363396+vfaels@users.noreply.github.com>
2025-06-23 13:51:26 +0000

    feat: melhorar configuração de CORS para permitir origens específicas

--- ecoenergia-api/server.js
+++ ecoenergia-api/server.js
@@ -4,11 +4,24 @@ require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const allowedOrigins = [
+  process.env.CORS_ORIGIN, 
+  'http://localhost:5173',   
+  'https://snack-web-player.s3.us-west-1.amazonaws.com' 
+];
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'A política de CORS para este site não permite acesso da Origem especificada.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
